d5b504d1e45007e1057aabedae958196017b0dfc
公司电脑pycharm <1728833205@qq.com>
2021-12-28 17:24:36 +0800

    新增了很多新内容

--- PDF.py
+++ PDF.py
@@ -1,8 +1,7 @@
 
 #本文件目标——创建PDF转图片的类——进一步的话可以对PDF进行进一步操作比如合并、提取文字等
 import os
-from PIL import Image, ImageEnhance
-import PIL
+from PyPDF2 import PdfFileReader, PdfFileWriter
 import tkinter
 import tkinter.filedialog
 import tkinter.messagebox
@@ -16,7 +15,7 @@ class Window():
     def __init__(self):#初始化操作——生成窗口、按钮等
         self.root = root = tkinter.Tk()
 
-        self.label = tkinter.Label(root, text='选择目录')
+        self.label = tkinter.Label(root, text='选择目录')#这里的选择目录应当是选择文件
         self.label.grid(row=3, column=0)
         self.entryDir = tkinter.Entry(root)
         self.entryDir.grid(row=3, column=1)
@@ -27,7 +26,7 @@ class Window():
         self.ButtonCov.grid(row=4, column=0)
 
     def BrowserDir(self):
-        directory = tkinter.filedialog.askdirectory(title='Python')#值即选择目录路径
+        directory = tkinter.filedialog.asksaveasfilename(title='Python')#值即选择文件路径
         if directory:#输出框内容
             self.entryDir.delete(0, tkinter.END)
             self.entryDir.insert(tkinter.END, directory)
@@ -38,24 +37,24 @@ class Window():
         #进度条，嵌套在上个函数里
         def _prog():
             top = tkinter.Toplevel(self.root)
-            top.attributes("-fullscreen", 1)
+            # top.attributes("-", 1)
             top.attributes("-topmost", 1)
             top.title("进度条")
-            tkinter.Label(top, text="正在处理图片，请稍候…").pack()
+            tkinter.Label(top, text="正在处理PDF，请稍候…").pack()
             prog = Progressbar(top, mode="indeterminate")#直接实例化ttk的方法
 
             prog.pack()
             prog.start()#进度条开始
             #转换
-            for file in os.listdir(path):
-                if file[-4:] in ('.bmp', '.jpg', 'jpeg', '.gif', '.png', '.BMP', '.JPG', 'JPGE', '.GIF', '.PNG'):
-                    f_img = path + "/" + file#新图片保存路径
-                    image = Image.open(f_img)
-                    newwidth = int(image.size[0] / 2)
-                    newheight = int(image.size[1] / 2)
-                    image = image.resize((newwidth, newheight), PIL.Image.NEAREST)#图片改为一半大小
-                    image.save(f_img)
-                    sleep(0.5)
+
+            pdf = PdfFileReader(path)
+            for page in range(pdf.getNumPages()):
+                pdf_writer = PdfFileWriter()
+                pdf_writer.addPage(pdf.getPage(page))
+                output = f'D:/base/temp/NO{page}.pdf'
+                with open(output, 'wb') as output_pdf:
+                    pdf_writer.write(output_pdf)
+            sleep(0.5)
             prog.stop()#进度条结束
             top.destroy()
             tkinter.messagebox.showinfo("提示：", "已完成！")
@@ -66,7 +65,7 @@ class Window():
     def mainloop(self):
         self.root.minsize(330, 190)
         self.root.maxsize(330, 170)
-        self.root.title('图片批量处理器')
+        self.root.title('PDF处理器')
         self.root.mainloop()
 
 
